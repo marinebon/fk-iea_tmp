7b09878c2239c7f32df42a6266e23340ed67eb75
Tylar Murray <mail@tylar.info>
2019-10-02 17:41:31 -0400

    + (untested) ERR printouts for svg element get fails

--- docs/libs/infographiq.js
+++ docs/libs/infographiq.js
@@ -22,80 +22,82 @@ function basename(path) {
 
 // main function to link svg elements to modal popups with data in csv
 function link_svg(svg, csv, debug = false, hover_color = 'yellow', width = '100%', height = '100%', modal_id = 'modal') {
-  
+
 
   //  var f_child = div.node().appendChild(f.documentElement);
   d3.xml(svg).then((f) => {
     // https://gist.github.com/mbostock/1014829#gistcomment-2692594
-  
+
     //var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d; });
-    
+
     var div = d3.select('#svg');
-  
+
     var f_child = div.node().appendChild(f.documentElement);
-    
+
     // get handle to svg
     var h = d3.select(f_child);
-    
+
     // full size
     h.attr('width', width)
      .attr('height', height);
-    
-    if (debug){ 
+
+    if (debug){
       console.log('before data.forEach');
     }
-    
+
     d3.csv(csv).then(function(data) {
-      
-      if (debug){ 
+
+      if (debug){
         console.log("data before filter");
         console.log(data);
       }
-      
+
       data = data.filter(function(d){ return basename(d.svg) == basename(svg) });
-      
-      if (debug){ 
+
+      if (debug){
         console.log("data after filter");
         console.log(data);
       }
 
       // iterate through rows of csv
       data.forEach(function(d) {
-        if (debug){ 
+        d.id = d.id.trim()
+
+        if (debug){
           console.log('forEach d.id: ' + d.id);
         }
-        
+
         function handleClick(){
           if (d.not_modal == 'T'){
             window.location = d.link;
           } else {
-            
-            if (debug){ 
+
+            if (debug){
               console.log('  link:' + d.link);
             }
-            
+
             $('#'+ modal_id).find('iframe')
               .prop('src', function(){ return d.link });
-            
+
             $('#'+ modal_id + '-title').html( d.title );
-            
+
             $('#'+ modal_id).on('show.bs.modal', function () {
               $('.modal-content').css('height',$( window ).height()*0.9);
               $('.modal-body').css('height','calc(100% - 65px - 55.33px)');
             });
-            
+
             $('#'+ modal_id).modal();
           }
         }
         function handleMouseOver(){
-          if (debug){ 
+          if (debug){
               console.log('  mouseover():' + d.id);
           }
-           
+
           d3.select('#' + d.id)
             .style("stroke-width", 2)
             .style("stroke", hover_color);
-          
+
           tooltip_div.transition()
             .duration(200)
             .style("opacity", 0.8);
@@ -104,47 +106,52 @@ function link_svg(svg, csv, debug = false, hover_color = 'yellow', width = '100%
             .style("top", (d3.event.pageY - 28) + "px");
         }
         function handleMouseOverSansTooltip(){
-          if (debug){ 
+          if (debug){
               console.log(' handleMouseOverSansTooltip():' + d.id);
           }
-           
+
           d3.select('#' + d.id)
             .style("stroke-width", 2)
             .style("stroke", hover_color);
-          
+
         }
         function handleMouseOut(){
-          if (debug){ 
+          if (debug){
               console.log('  mouseout():' + d.id);
             }
-            
+
             //d3.select(this)
             d3.select('#' + d.id)
               .style("stroke-width",0);
-  
+
             tooltip_div.transition()
               .duration(500);
             tooltip_div.style("opacity", 0);
         }
-        
-        if (debug){ 
+
+        if (debug){
           console.log('before .on(): ' + d.id);
         }
 
-        h.select('#' + d.id)
-          .on('click', handleClick)
+        element = h.select('#' + d.id)
+        if (element._groups == undefined || element._groups.length == 0){
+            console.log('ERR: could not get svg element "' + d.id + '"')
+        } else if ( element._groups.length > 1 ) {
+            console.log('ERR: got > 1 svg element for "' + d.id + '"')
+        }
+        element.on('click', handleClick)
           .on('mouseover', handleMouseOver)
           .on('mouseout', handleMouseOut);
-        
-        if (debug){ 
+
+        if (debug){
           console.log('after .on(): ' + d.id);
         }
-        
+
         // set outline of paths within group to null
         d3.select('#' + d.id).selectAll("path")
             .style("stroke-width", null)
             .style("stroke", null);
-          
+
         // add to bulleted list of svg elements
         list_text = d.title ? d.title : d.id;  // fall back on id if title not set
         d3.select("#svg_list").append("li").append("a")
@@ -153,16 +160,16 @@ function link_svg(svg, csv, debug = false, hover_color = 'yellow', width = '100%
           .on('mouseover', handleMouseOverSansTooltip)
           .on('mouseout', handleMouseOut);
 
-        if (debug){ 
+        if (debug){
           console.log('end forEach d.id: ' + d.id);
         }
-      
+
       }); // end: data.forEach({
     }) // end: d3.csv().then({
     .catch(function(error){
-      // d3.csv() error   
+      // d3.csv() error
     }); // end: d3.csv()
-    
+
   // turn off questions by default
   d3.select("#text").attr("display", "none");
 
